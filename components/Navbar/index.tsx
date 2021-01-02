01cf493e0616749b678b32131057f7c2bba74945
sajalrs <sajalsatyal@gmail.com>
2021-01-02 13:38:46 -0600

    configured sass adn added css file to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,28 +1,44 @@
 import React from "react";
 import Link from "next/link";
-
+import navbarStyles from "./navbar.module.css";
 const Navbar = () => {
   return (
+    <div className={navbarStyles["navbar"]}>
     <nav>
-      <Link href="/">
-        <a>Programs {"&"} Services</a>
-      </Link>
-      <Link href="/">
-        <a>Partnerships</a>
-      </Link>
-      <Link href="/">
-        <a>Photo Gallery</a>
-      </Link>
-      <Link href="/">
-        <a>Testimonials</a>
-      </Link>
-      <Link href="/">
-        <a>Contact Us</a>
-      </Link>
-      <Link href="/">
-        <a>Register</a>
-      </Link>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Programs {"&"} Services</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Partnerships</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Photo Gallery</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Testimonials</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Contact Us</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Register</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
+    </div>
   );
 };
 
