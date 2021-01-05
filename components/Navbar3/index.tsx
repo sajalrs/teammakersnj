e7ada75fddb9ebb6e73a1c0936a9baabfc317622
sajalrs <sajalsatyal@gmail.com>
2021-01-04 18:36:37 -0600

    new navbar responsive and functional atleast

--- components/Navbar3/index.tsx
+++ components/Navbar3/index.tsx
@@ -12,63 +12,67 @@ const Navbar = () => {
             alt="Team Makers of New Jersey Logo"
           />
 
-          {isActive ? (
-            <span
-              className="fas fa-times-circle"
-              onClick={() => {
-                setActivity(false);
-              }}
-            />
-          ) : (
-            <span
-              className="fas fa-list"
-              onClick={() => {
-                setActivity(true);
-              }}
-            />
-          )}
-          <ul
-            className={
-              isActive
-                ? `${navbarStyles["menu"]} ${navbarStyles["is-active"]}`
-                : navbarStyles["menu"]
-            }
-          >
-            <li className={navbarStyles["menu-item"]}>Home</li>
-            <li className={navbarStyles["menu-item"]}>About Us</li>
-            <li
-              className={`${navbarStyles["menu-item"]} ${navbarStyles["has-sub-menu"]}`}
+          <div className={navbarStyles["hamburger-and-menu"]}>
+            {isActive ? (
+              <span
+                className={`${navbarStyles["hamburger"]} fas fa-times-circle`}
+                onClick={() => {
+                  setActivity(false);
+                }}
+              />
+            ) : (
+              <span
+                className={`${navbarStyles["hamburger"]} fas fa-list`}
+                onClick={() => {
+                  setActivity(true);
+                }}
+              />
+            )}
+            <ul
+              className={
+                isActive
+                  ? navbarStyles["menu"]
+                  : `${navbarStyles["menu"]} ${navbarStyles["is-inactive"]}`
+              }
             >
-              Programs {"&"} Services
-              <ul className={navbarStyles["sub-menu"]}>
-                <li className={navbarStyles["sub-menu-item"]}>
-                  After-School Programs
-                </li>
-                <li className={navbarStyles["sub-menu-item"]}>
-                  Birthday Parties
-                </li>
-                <li className={navbarStyles["sub-menu-item"]}>Camps</li>
-                <li className={navbarStyles["sub-menu-item"]}>Workshops</li>
-                <li className={navbarStyles["sub-menu-item"]}>
-                  Carnivals and Fun Days
-                </li>
-                <li className={navbarStyles["sub-menu-item"]}>Assemblies</li>
-              </ul>
-            </li>
-            <li className={navbarStyles["menu-item"]}>Partnerships</li>
-            <li className={navbarStyles["menu-item"]}>Photo Gallery</li>
-            <li className={navbarStyles["menu-item"]}>Testimonials</li>
-            <li
-              className={`${navbarStyles["menu-item"]} ${navbarStyles["has-sub-menu"]}`}
-            >
-              Contact Us
-              <ul className={navbarStyles["sub-menu"]}>
-                <li className={navbarStyles["sub-menu-item"]}>Contact Info</li>
-                <li className={navbarStyles["sub-menu-item"]}>Careers</li>
-              </ul>
-            </li>
-            <li className={navbarStyles["menu-item"]}>Register</li>
-          </ul>
+              <li className={navbarStyles["menu-item"]}>Home</li>
+              <li className={navbarStyles["menu-item"]}>About Us</li>
+              <li
+                className={`${navbarStyles["menu-item"]} ${navbarStyles["has-sub-menu"]}`}
+              >
+                Programs {"&"} Services
+                <ul className={navbarStyles["sub-menu"]}>
+                  <li className={navbarStyles["sub-menu-item"]}>
+                    After-School Programs
+                  </li>
+                  <li className={navbarStyles["sub-menu-item"]}>
+                    Birthday Parties
+                  </li>
+                  <li className={navbarStyles["sub-menu-item"]}>Camps</li>
+                  <li className={navbarStyles["sub-menu-item"]}>Workshops</li>
+                  <li className={navbarStyles["sub-menu-item"]}>
+                    Carnivals and Fun Days
+                  </li>
+                  <li className={navbarStyles["sub-menu-item"]}>Assemblies</li>
+                </ul>
+              </li>
+              <li className={navbarStyles["menu-item"]}>Partnerships</li>
+              <li className={navbarStyles["menu-item"]}>Photo Gallery</li>
+              <li className={navbarStyles["menu-item"]}>Testimonials</li>
+              <li
+                className={`${navbarStyles["menu-item"]} ${navbarStyles["has-sub-menu"]}`}
+              >
+                Contact Us
+                <ul className={navbarStyles["sub-menu"]}>
+                  <li className={navbarStyles["sub-menu-item"]}>
+                    Contact Info
+                  </li>
+                  <li className={navbarStyles["sub-menu-item"]}>Careers</li>
+                </ul>
+              </li>
+              <li className={navbarStyles["menu-item"]}>Register</li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
