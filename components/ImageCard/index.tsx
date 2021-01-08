da9dabd55564881f6ed31b87c54367adc95836dd
sajalrs <sajalsatyal@gmail.com>
2021-01-07 19:31:53 -0600

    added text position enum ot Image Card and lifted data out of ImageCard component

--- components/ImageCard/index.tsx
+++ components/ImageCard/index.tsx
@@ -1,22 +1,46 @@
+import { totalmem } from "os";
 import React from "react";
-import GreenButton from "../Buttons/Green"
-import TransparentButton from "../Buttons/Transparent"
+import GreenButton from "../Buttons/Green";
+import TransparentButton from "../Buttons/Transparent";
+import imageCardStyles from "../ImageCard/ImageCard.module.css";
 
-const ImageCard = () => {
-  return <div>
-      <img src="/TEAM-MAKERS-PHOTO-2-1.jpg"/>
-      <div>
-          <h1>CAMP ADVENTURES</h1>
-          <h2>Bring Teamwork to you Camp</h2>
-          <div>
+export enum TextPosition {
+    TopLeft = "top-left",
+    TopCenter = "top-center",
+    TopRight = "top-right",
+    MiddleLeft = "middle-left",
+    MiddleCenter = "middle-center",
+    MiddleRight = "middle-right",
+    BottomLeft = "bottom-left",
+    BottomCenter = "bottom-center",
+    BottomRight = "bottom-right"
+}
 
-          </div>
-          <div>
-            <GreenButton buttonText="LEARN MORE" onClick={()=>{}}/>
-            <TransparentButton buttonText="CONTACT US" onClick={()=>{}}/>
-          </div>
+type Props = {
+    textPosition: TextPosition,
+    imgSrc: string,
+    title: string,
+    subtitle: string
+}
+
+
+const ImageCard = ({textPosition, imgSrc, title, subtitle}: Props) => {
+  return (
+    <div className={imageCardStyles["image-card"]}>
+      <img
+        id={imageCardStyles["image-card-image"]}
+        src={imgSrc}
+      />
+      <div className={`${imageCardStyles["image-card-text"]} ${imageCardStyles[textPosition]} `}>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <div className={imageCardStyles["buttons"]}>
+          <GreenButton buttonText="LEARN MORE" onClick={() => {}} />
+          <TransparentButton buttonText="CONTACT US" onClick={() => {}} />
+        </div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default ImageCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import ImageCard from "../components/ImageCard";
-
+import {TextPosition} from "../components/ImageCard"
 const IndexPage = () => (
   <Layout title="Team Makers of North Jersey" isNavbarTransparent={true}>
-      <ImageCard/>
+      <ImageCard textPosition={TextPosition.BottomCenter} imgSrc="/TEAM-MAKERS-PHOTO-2-1.jpg" title="CAMP ADVENTURES" subtitle="Bring Teamwork to you Camp"/>
       <div style={{ height: "1000px" }}></div>
     
+
+        
   </Layout>
 );
 
